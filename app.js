a91bb3be0eba295c4f70bee84ed6f7510c100a9e
JasmineKhalimova <jasmine.khalimova@yahoo.com>
2020-06-20 20:26:29 +0100

    modified app js file

--- app.js
+++ app.js
@@ -1,50 +1,44 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import path from 'path';
+import cors from 'cors';
 import bodyParser from 'body-parser';
-const mongoose = require("mongoose");// DB connection
-const morgan = require("morgan");// using morgan as middlewares
-const cookieParser = require("cookie-parser");// to save user credentials in the cookie
-const cors = require("cors");// to handle api request from different origin
-
+import morgan from 'morgan';
+import config from './config';
 
 // routes
 import authRoutes from './routes/api/auth';
 import itemRoutes from './routes/api/items';
 import userRoutes from './routes/api/users';
 
-require("dotenv").config();
+const { MONGO_URI, MONGO_DB_NAME } = config;
 
 const app = express();
 
 // CORS Middleware
 app.use(cors());
-
 // Logger Middleware
 app.use(morgan('dev'));
-
 // Bodyparser Middleware
 app.use(bodyParser.json());
 
-//app
-const app = express();
+// DB Config
+const db = `${MONGO_URI}/${MONGO_DB_NAME}`;
 
-//DB connection
-mongoose.connect(process.env.DATABASE,{
+// Connect to Mongo
+mongoose
+  .connect(db, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
-}).then(()=>console.log('DB Connected'));
+  }) // Adding new mongo url parser
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
 
 // Use Routes
 app.use('/api/items', itemRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
-
-//Middlewares
-app.use(morgan('dev'));
-app.use(bodyParser.json());
-app.use(cookieParser());
-app.use(cors());
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
